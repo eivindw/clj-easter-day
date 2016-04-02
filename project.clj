c03c7cd9c97ebb47e4a7c152e33ffb2e4ce06e39
eivindw <eivindw@gmail.com>
2016-04-02 17:44:27 +0200

    Fix website url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject easter-day "0.2.0-SNAPSHOT"
   :description "Function to find day and month of Easter Sunday given year."
-  :url "http://github.com/eivind/easter-day"
+  :url "https://github.com/eivindw/clj-easter-day"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
