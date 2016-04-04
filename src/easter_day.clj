775ff7a87b7e82e4ae84a7e7d433dd33be773975
eivwaale <eivind.waaler@finn.no>
2016-04-04 07:58:34 +0200

    Simplify expressions.

--- src/easter_day.clj
+++ src/easter_day.clj
@@ -10,10 +10,10 @@
         e (mod b 4)
         f (quot (+ b 8) 25)
         g (quot (+ (- b f) 1) 3)
-        h (mod (+ (* 19 a) (- (- b d) g) 15) 30)
+        h (mod (+ (* 19 a) (- b d g) 15) 30)
         i (quot c 4)
         k (mod c 4)
-        l (mod (- (- (+ 32 (* 2 e) (* 2 i)) h) k) 7)
+        l (mod (- (+ 32 (* 2 e) (* 2 i)) h k) 7)
         m (quot (+ a (* 11 h) (* 22 l)) 451)
         n (quot (+ h (- l (* 7 m)) 114) 31)
         p (mod (+ h (- l (* 7 m)) 114) 31)]
