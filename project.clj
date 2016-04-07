31ccd6df6f0df2609cfe807afd5196af0b007733
eivwaale <eivind.waaler@finn.no>
2016-04-07 09:27:43 +0200

    Set default ns for repl.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :deploy-repositories [["releases" :clojars]
-                        ["snapshots" :clojars]])
+                        ["snapshots" :clojars]]
+  :repl-options {:init-ns easter-day})
