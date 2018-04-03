071c1be7aa7dd36e580e9770539eb09083638275
Odin Standal <odin@odinodin.com>
2018-04-03 10:58:28 +0200

    Mark Clojure as a dev dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/eivindw/clj-easter-day"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
   :repl-options {:init-ns easter-day})
